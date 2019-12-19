d678a235934401453586f5f20691eb863c6d7e62
Cauldierre McKay <cxm1544@rit.edu>
2019-12-18 21:56:56 -0500

    Updates

---
+++ precache-manifest.626052081b2bdeb32233bdeee18dbff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c4826bc95048720190a7ce6d683811",
+    "url": "/folio/index.html"
+  },
+  {
+    "revision": "120f3962c03cb332983f",
+    "url": "/folio/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "5791988b055e3503cbf9",
+    "url": "/folio/static/js/2.a0a80744.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/folio/static/js/2.a0a80744.chunk.js.LICENSE"
+  },
+  {
+    "revision": "120f3962c03cb332983f",
+    "url": "/folio/static/js/main.a4476390.chunk.js"
+  },
+  {
+    "revision": "fcf45c29f7b042252740",
+    "url": "/folio/static/js/runtime-main.a41331dd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/folio/static/media/logo.25bf045c.svg"
+  }
+]);
