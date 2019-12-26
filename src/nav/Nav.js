b24aadd145be923b8b62d64312a17da3bdcade29
Cauldierre McKay <cxm1544@rit.edu>
2019-12-26 15:44:53 -0500

    Starting to do some type of components
    blah. mainly just moving out the way to work on somthing else

---
+++ src/nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Nav.scss';
+
+/**
+ * 5 Specific projects
+ * More projects in a drop down to the side
+ * Apple nav it maybe by having at bottom of home screen
+ * then shifting to top (granted no clue what's on homescreen)
+ */
+class Nav extends React.Component{
+    
+}
+export default Nav;
---
+++ src/resume/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Resume.scss';
+
+/**
+ * Class created to display the file that is uploaded
+ * Specifically a pdf file
+ * If no file uploaded, ask to import a file
+ * if file not .pdf suffix, error and show no file screen again
+ */
+class Resume extends React.Component{
+    
+}
+export default Resume;
